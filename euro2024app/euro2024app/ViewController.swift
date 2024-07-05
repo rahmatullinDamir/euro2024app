1cb23ab50b455c1247176a7bf02ff7d3c8ac6932
Damir Rakhmatullin <damirrakhmatuliin@yandex.ru>
2024-07-05 22:19:09 +0400

    done first View

--- euro2024app/euro2024app/ViewController.swift
+++ euro2024app/euro2024app/ViewController.swift
@@ -7,13 +7,92 @@
 
 import UIKit
 
+class UnderlineLayer: CALayer {
+    
+    var inputHeight: CGFloat
+    var inputWidth: CGFloat
+    
+    init(inputElem: UIStackView) {
+        self.inputHeight = inputElem.frame.height - 1
+        self.inputWidth = inputElem.frame.width - 1
+        super.init()
+    }
+    
+    func getCALayer() -> CALayer {
+        var outputCALayer = CALayer()
+        outputCALayer.frame = CGRect(x: 0, y: inputHeight, width: inputWidth, height: 1)
+        outputCALayer.backgroundColor = UIColor.gray.cgColor
+        
+        return outputCALayer
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var groupCompetition: UIStackView!
+    @IBOutlet weak var final: UIStackView!
+    @IBOutlet weak var halfFinal: UIStackView!
+    @IBOutlet weak var quaterFinal: UIStackView!
+    @IBOutlet weak var odnavosmayaFinal: UIStackView!
+    @IBOutlet weak var matchesView: UIView!
+    @IBOutlet weak var competitionTableView: UIView!  
+    
+    @IBOutlet weak var tournamentTableView: UITableView!
+    var dataSource: [String] = ["Группа A", "Группа B", "Группа C", "Группа D", "Группа E", "Группа F"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        matchesView.layer.cornerRadius = 25
+        
+        
+        var ur1 = UnderlineLayer(inputElem: final)
+        final.layer.addSublayer(ur1.getCALayer())
+        var ur2 = UnderlineLayer(inputElem: halfFinal)
+        halfFinal.layer.addSublayer(ur2.getCALayer())
+        var ur3 = UnderlineLayer(inputElem: quaterFinal)
+        quaterFinal.layer.addSublayer(ur3.getCALayer())
+        var ur4 = UnderlineLayer(inputElem: odnavosmayaFinal)
+        odnavosmayaFinal.layer.addSublayer(ur4.getCALayer())
+        var ur5 = UnderlineLayer(inputElem: groupCompetition)
+        groupCompetition.layer.addSublayer(ur5.getCALayer())
+        
+        competitionTableView.layer.cornerRadius = 25
+        tournamentTableView.backgroundColor = UIColor .clear
+       
+        tournamentTableView.dataSource = self
+        tournamentTableView.delegate = self
+        
+        
         // Do any additional setup after loading the view.
+        
     }
 
 
+}
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell") as? TableViewCell else {return UITableViewCell()}
+        
+        cell.config(nameOfLable: dataSource[indexPath.row])
+        cell.backgroundColor = UIColor .clear
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+    }
+    
+    
+    
 }
 
