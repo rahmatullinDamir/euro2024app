9c12783638963ff53fc64913c44d01ee1e4cb543
rahmatullinDamir <rahmatullin.damir21@gmail.com>
2024-07-05 14:52:20 +0400

    storyboard created

---
+++ euro2024app/euro2024app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  euro2024app
+//
+//  Created by Damir Rakhmatullin on 5.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
